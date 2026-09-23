078de6321fced14aa212c0f2baa44b2fcc6b8866
DKapoore <144426124+DKapoore@users.noreply.github.com>
2026-09-23 14:46:30 +0530

    Update config.js

--- admin/config.js
+++ admin/config.js
@@ -3,7 +3,7 @@
 // This must be the SAME URL used in customer/config.js - it is one
 // backend serving both apps.
 // ---------------------------------------------------------------------
-const API_URL = "https://script.google.com/macros/s/AKfycbyn0Jw4goHi9KmPIHShBdgASptVm1OzyaeToOOzNznp-Tf1VfLuZLIkv1UwY2pZdKqD/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbz6TmrXX1kZuceuO29ZtzbcxSVFc7DNb3ZAey4a0iJoF7rxMkjgNTqA1XMhxOgbzrf-MQ/exec";
 
 // ---------------------------------------------------------------------
 // OneSignal App ID for admin push notifications (New Booking, Payment
